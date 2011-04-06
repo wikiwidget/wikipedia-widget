4959aa657c315431328dd2b332e681f41f325bd6
sbillig <sean.billig@gmail.com>
2011-04-06 01:03:48 -0500

    slow down mouse wheel scroll speed

--- Wikipedia.wdgt/Scripts/Scroller.js
+++ Wikipedia.wdgt/Scripts/Scroller.js
@@ -209,7 +209,7 @@ function hideScrollBar() {
 
 function mouseWheelMove(event) {
     if (scrollable) {
-        var clicks = window.event.wheelDelta/10;
+        var clicks = window.event.wheelDelta/100;
         scrollBy(clicks);
     }
 }
